2af51bafa9021da79d1dfe7d9a230abb38a9f3fb
MartiPuigvi <marti.puigvi@cirvianum.cat>
2026-09-16 20:31:40 +0200

    Repte 0 fet

---
+++ hola_mon_js/script.js
@@ -0,0 +1,7 @@
+function saluda() {
+    let missatge = "";
+    missatge = document.getElementById('hola').value; // Crida l'id de l'input i agafa el valor que poses al quadre de text
+    alert("Hola món! Has clicat el botó! " + missatge); //Mostra el missatge per defecte i el que has introduit al quadre de text
+    console.log("Hola món! Has clicat el botó!");
+
+}
